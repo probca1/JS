ba7a2d6e70adb3cd746f623c9ce3d1ecdb81720f
Surendra <129713311+probca1@users.noreply.github.com>
2023-12-01 13:01:09 +0545

    update

--- js/data.js
+++ js/data.js
@@ -3,23 +3,46 @@
 => The latest ECMAScript standard defines eight data types Out of which seven data types are Primitive(predefined) and one complex or Non-Primitive.
 */
 
-"use strict" //treat all JS as newer version
+// "use strict" //treat all JS as newer version
 //alert() // => we are using nodejs that doesn't support, works only on browser or old version
 
 /* lets talk abt readability/ future proved, apps developer avoid ";"
 better to write separate line and auto wrap of line. we can use 'Prettier code formatter"
 get help from MDN and ECMA.org.
 */
+// https://262.ecma-international.org/5.1/#sec-11.4.3
 
-let age=25 //number
-let weight=62.5  //number
-let name="Surendra"  //string -> we can use single / double quotes
-let isTrue= true //boolean => true/false
-let provience=null //=>empty / standalone value
-let country;    //undefined => not assigned
-let brand("sptcode") //symbol=> mostly used in react/ identify or denote uniquely.
-// object => the most important: array, set , null etc.
+//  7 types : String, Number, Boolearn, null, undefined, Symbol, BigInt
+
+// let age=25 //number
+// let weight=62.5  //number
+// let name="Surendra"  //string -> we can use single / double quotes
+// let isTrue= true //boolean => true/false
+// let provience=null //=>empty / standalone value
+// let country;    //undefined => not assigned
+// let brand("sptcode") //symbol=> mostly used in react/ identify or denote uniquely.
+// const id = Symbol('123')
+// const anotherId = Symbol('123')
+// console.log(id === anotherId);
 
 //typeof function:
-console.log(typeof age);
-console.log(typeof (age);
+// console.log(typeof age);
+// console.log(typeof (age);
+
+// <<<<< Reference (Non primitive)>>>>>>
+// Array, Objects, Functions
+
+// const heros = ["shaktiman", "naagraj", "doga"];
+// let myObj = {
+//     name: "hitesh",
+//     age: 22,
+// }
+
+// const myFunction = function(){
+//     console.log("Hello world");
+// }
+
+// console.log(typeof anotherId);
+
+
+
